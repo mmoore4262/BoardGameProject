104f2f3d77437b09ca8c5133680a9971b2dd4548
BT_1N3_09 <mmoore4262@bths.edu>
2018-11-01 10:13:20 -0400

    boardspaces class initialized.

---
+++ src/BoardSpaces.java
@@ -0,0 +1,15 @@
+// Create a double Boardspaces array; Boardspaces[][], and name it board. Initialize the array- board, with any ints. 5,5; 10,6; 8,32; etc. Fill the board with boardspaces
+public class BoardSpaces {
+    //fields
+    // x and y are the number of horizontal and vertical spaces of the board.
+    //Will probably need a player field soon.
+    private int x;
+    private int y;
+    public BoardSpaces (int x, int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    // need to think of a theme for the board game before I create new classes
+
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,19 @@
 public class Runner {
+    private static boolean gameOn = true;
+
+    public static void main(String[] args)
+    {
+        //initialize board to hold any number of BoardSpaces
+        BoardSpaces[][] board = new BoardSpaces[10][5];
+        //fill the board indexes with normal BoardSpaces
+        for (int x=0; x<board.length; x++)
+        {
+            for (int y=0; y<board[x].length; y++)
+            {
+                //board now holds [i] sets of [j] sets of BoardSpaces
+                board[x][y]=new BoardSpaces(x,y);
+            }
+        }
+
+    }
 }
